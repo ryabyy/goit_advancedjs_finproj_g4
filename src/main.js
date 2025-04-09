a58e20b5ea5e6b7cacab68c4a3c11292d892e691
Serhiy R. <s.ryabyy@gmail.com>
2025-04-08 17:51:46 -0700

    added API+Local services, some JS structure, axios+izitoast and optimized the favicon

---
+++ src/main.js
@@ -0,0 +1,53 @@
+import iziToast from 'izitoast';
+import 'izitoast/dist/css/iziToast.min.css';
+import { ApiService, StorageService, ApiError } from './js/services';
+
+///////////////////// API + Local Usage examples: /////////////////////
+
+// // StorageService.clearDailyQuote();
+// // StorageService.clearFavorites();
+
+// const quote = await StorageService.loadDailyQuote();
+// console.log(quote);
+
+// const favorites = StorageService.loadFavorites();
+// console.log(favorites);
+
+// const filters = await ApiService.fetchFilters('Body parts', 1, 10);
+// const exercises = await ApiService.fetchExercises('back', '', '', 'ups', 1, 10);
+// const exercise = await ApiService.fetchExerciseByID('64f389465ae26083f39b17a2');
+
+// favorites.push(exercise);
+// StorageService.storeFavorites(favorites);
+
+// try {
+//   const res = await ApiService.addSubscription('test@gmail.com');
+// } catch (error) {
+//   if (error instanceof ApiError) {
+//     if (error.statusCode === 409) {
+//       iziToast.warning({
+//         message: 'Provided email is already subscribed!',
+//         position: 'topCenter',
+//       });
+//     }
+//   }
+// }
+
+// try {
+//   const res = await ApiService.submitExerciseRating(
+//     '64f389465ae26083f39b17a2',
+//     5,
+//     'test@gmail.com'
+//   );
+// } catch (error) {
+//   if (error instanceof ApiError) {
+//     if (error.statusCode === 409) {
+//       iziToast.warning({
+//         message: 'This email already used to rate the exercise!',
+//         position: 'topCenter',
+//       });
+//     }
+//   }
+// }
+
+///////////////////// End of API + Local examples: /////////////////////
