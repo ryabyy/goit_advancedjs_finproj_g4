fc684a5310ff0599031dce3713feb3ce9be36501
Yuriy Borys <iam618384@gmail.com>
2025-04-09 10:34:00 +0300

    Added quote component

--- src/js/quote.js
+++ src/js/quote.js
@@ -1,6 +1,15 @@
+// Mock data
 const quote = {
   author: "Muhammad Ali",
   quote: "I hated every minute of training, but I said, 'Don't quit. Suffer now and live the rest of your life as a champion.'"
 };
 
+const textQuote = document.querySelector('.daily-text__text-quote');
+const textAuthor = document.querySelector('.daily-text__text-author');
 
+export function updateQuote({ quote, author }) {
+  textQuote.style.innerText = quote;
+  textAuthor.style.innerText = author;
+  // In case the next quote will fail to fetch, we can use the previous one from localStorage
+  localStorage.setItem('quote', JSON.stringify({ quote, author }));
+}
