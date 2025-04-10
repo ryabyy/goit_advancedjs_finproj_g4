fe1c8468da26f7800d56a1ff4948cb5d09454c11
Yuriy Borys <iam618384@gmail.com>
2025-04-10 10:24:39 +0300

    Renamed card classes

--- src/js/exer-card.js
+++ src/js/exer-card.js
@@ -92,15 +92,15 @@ let isFavoritesCart = false;
 export function updateList(list) {
   isFavoritesCart = !!listElem.dataset.mode;
   list.forEach(({ name, burnedCalories, bodyPart, target, rating }) => {
-    const item = getItem();
-    item.querySelector('.exer-item__name__text').innerText = truncateString(name[0].toUpperCase() + name.slice(1), 34);
-    item.querySelector('.cal span').innerText = truncateString(burnedCalories.toString(), 5);
-    item.querySelector('.part span').innerText = truncateString(bodyPart, 5);
-    item.querySelector('.target span').innerText = truncateString(target, 5);
+    const card = getCard();
+    card.querySelector('.exer-card__name__text').innerText = truncateString(name[0].toUpperCase() + name.slice(1), 34);
+    card.querySelector('.cal span').innerText = truncateString(burnedCalories.toString(), 5);
+    card.querySelector('.part span').innerText = truncateString(bodyPart, 5);
+    card.querySelector('.target span').innerText = truncateString(target, 5);
     if (!isFavoritesCart) {
-      item.querySelector('.exer-item__post-tag span').innerText = numberToString(rating);
+      card.querySelector('.exer-card__post-tag span').innerText = numberToString(rating);
     }
-    listElem.append(item);
+    listElem.append(card);
   });
 }
 // TODO: to be removed
@@ -110,9 +110,9 @@ function onRemove() {}
 
 function onStart() {}
 
-function getItem() {
+function getCard() {
   const li = document.createElement('li');
-  li.classList.add('exer-list__item');
+  li.classList.add('exer-list__card');
 
   // Item header
   const header = document.createElement('div');
@@ -120,12 +120,12 @@ function getItem() {
   const tag = document.createElement('div');
   const postTag = getPostTagItem();
   const startBtn = document.createElement('button');
-  header.classList.add('exer-item__header');
-  tagBox.classList.add('exer-item__tag-box');
-  tag.classList.add('exer-item__tag');
+  header.classList.add('exer-card__header');
+  tagBox.classList.add('exer-card__tag-box');
+  tag.classList.add('exer-card__tag');
   tag.innerText = 'WORKOUT';
-  postTag.classList.add('exer-item__post-tag');
-  startBtn.classList.add('exer-item__start-btn');
+  postTag.classList.add('exer-card__post-tag');
+  startBtn.classList.add('exer-card__start-btn');
   startBtn.innerText = 'Start';
   startBtn.append(getIcon('16', '16', 'arrow-right'));
   tagBox.append(tag, postTag);
@@ -136,15 +136,15 @@ function getItem() {
   const name = document.createElement('div');
   const icon = getIcon('24', '24', 'icon-running-man-black-bg');
   const nameText = document.createElement('p');
-  name.classList.add('exer-item__name');
-  icon.classList.add('exer-item__name__icon');
-  nameText.classList.add('exer-item__name__text');
+  name.classList.add('exer-card__name');
+  icon.classList.add('exer-card__name__icon');
+  nameText.classList.add('exer-card__name__text');
   name.append(icon, nameText);
   li.append(name);
 
   // Item stats
   const stats = document.createElement('div');
-  stats.classList.add('exer-item__stats');
+  stats.classList.add('exer-card__stats');
   STATS_ITEMS.forEach(({ name, className }) => {
     stats.append(getStatsElement(name, className));
   });
@@ -170,7 +170,7 @@ function getPostTagItem() {
 function getStatsElement(name, className) {
   const box = document.createElement('div');
   const span = document.createElement('span');
-  box.classList.add('exer-item__stats__param', className);
+  box.classList.add('exer-card__stats__param', className);
   box.innerText = `${name}: `;
   box.appendChild(span);
   return box;
