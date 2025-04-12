c186f5d265fa728be9c773d2f9dff1974ccb202e
v.karakai <v.karakai@pin-up.tech>
2025-04-12 10:53:56 +0300

    Added starting implementation

--- src/js/header.js
+++ src/js/header.js
@@ -2,6 +2,17 @@ const Container = document.querySelector('.categories-container');
 const Items = Container.querySelectorAll('.category');
 const ActiveItem = Container.querySelector('.category--active');
 const Switcher = Container.querySelector('.switcher');
+const burgerMenu = document.getElementById('burger-menu');
+const sideMenu = document.getElementById('side-menu');
+const sideMenuClose = document.getElementById('side-menu-close');
+
+burgerMenu.addEventListener('click', () => {
+  sideMenu.style.right = '0';
+});
+
+sideMenuClose.addEventListener('click', () => {
+  sideMenu.style.right = '-100%';
+});
 
 function moveSwitcherTo(item) {
   Switcher.style.width = `${item.offsetWidth}px`;
@@ -12,19 +23,19 @@ function isActiveItems(elements, activeElem, container) {
   if (activeElem) {
     moveSwitcherTo(activeElem);
   }
-  
+
   elements.forEach(element => {
     const link = element.querySelector('.category__link');
-    
+
     link.addEventListener('click', event => {
       event.preventDefault();
 
       container.querySelector('.category--active')?.classList.remove('category--active');
 
       element.classList.add('category--active');
-      
+
       moveSwitcherTo(element);
-      
+
       const href = link.getAttribute('href');
       setTimeout(() => {
         window.location.href = href;
